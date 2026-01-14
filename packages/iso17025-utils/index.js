3c6ba57f7f4d169f58cd882f42d9c6615fc898bd
rulos-nico <jariasnicosan@gmail.com>
2026-01-14 12:44:01 -0500

    borron y cuenta nueva

--- packages/iso17025-utils/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Utilidades para laboratorio ISO 17025
- * Paquete compartido entre frontend y backend
- */
-
-export { validarCodigoMuestra, validarRangoTemperatura } from './validations.js'
-export { calcularIncertidumbre, calcularDesviacionEstandar } from './calculations.js'
--- packages/shared-types/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Tipos y estructuras compartidas
- */
-
-export { ROLES, ESTADOS_ENTREGABLE, ESTADOS_MUESTRA, TIPOS_ENSAYO } from './enums.js'
-export { MENSAJES_ERROR, RUTAS_API } from './constants.js'
